1708f888de0e0bac09fdce79e2a119325c919957
Antonio <antonio.bozzano3@hotmail.com>
2023-05-17 20:17:17 -0300

    Refatoração componentes

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,17 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com 💜 sangue e suor no NLW da{' '}
+      <a
+        target="_blank"
+        className="underline hover:text-gray-100"
+        href="http://rocketseat.com.br"
+        rel="noreferrer"
+      >
+        {' '}
+        Rocketseat
+      </a>
+      🙌
+    </div>
+  )
+}
