bab38687c9955cbdd7dafbf23f90b802713a474b
Antonio <antonio.bozzano3@hotmail.com>
2023-05-18 01:39:29 -0300

    Auth front end

--- web/next.config.js
+++ web/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['avatars.githubusercontent.com'], // quais domínios vao ser permitidos
+  },
+}
 
 module.exports = nextConfig
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,18 +1,22 @@
 import { Hero } from '@/components/Hero'
 import { Copyright } from '@/components/Copyright'
 import { SignIn } from '@/components/SignIn'
+import { Profile } from '@/components/Profile'
 import { EmptyMemories } from '@/components/EmptyMemories'
 import { Stripes } from '@/components/Stripes'
 import { Blur } from '@/components/Blur'
 
+import { cookies } from 'next/headers'
+
 export default function Home() {
+  const isAuth = cookies().has('token')
   return (
     <main className="grid min-h-screen grid-cols-2">
       {/* left */}
       <div className=" relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
         <Blur /> {/* blur */}
         <Stripes /> {/* stripes */}
-        <SignIn /> {/* singn-in Aqui */}
+        {isAuth ? <Profile /> : <SignIn />} {/* Ternário logado ou não */}
         <Hero /> {/* Hero Aqui */}
         <Copyright /> {/* Copyright Aqui */}
       </div>
