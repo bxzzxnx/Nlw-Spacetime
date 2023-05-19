a812bc79cce19f64943b120527620481d18a44b3
Antonio <antonio.bozzano3@hotmail.com>
2023-05-19 19:25:25 -0300

    Media preview done 👍

---
+++ web/src/components/MediaPicker.tsx
@@ -0,0 +1,46 @@
+'use client'
+import { ChangeEvent, useState } from 'react'
+
+// o quanto menos possivel
+
+// encapsular o componente que precisa de interatividade
+// o max possivel
+export function MediaPicker() {
+  const [preview, setPreview] = useState<string | null>(null)
+
+  function onFileSelected(event: ChangeEvent<HTMLInputElement>) {
+    const { files } = event.target
+
+    if (!files) {
+      return
+    }
+
+    // 1 posição, n é input múltiplo
+    const previewURL = URL.createObjectURL(files[0])
+
+    setPreview(previewURL)
+  }
+
+  return (
+    <>
+      <input
+        onChange={onFileSelected}
+        type="file"
+        accept="image/*"
+        id="media"
+        className="invisible h-0 w-0"
+      />
+
+      {preview && (
+        // eslint-disable-next-line
+        <img
+          src={preview}
+          alt=""
+          className="aspect-video w-full rounded-lg object-cover"
+        />
+      )}
+
+      {/* video controls false */}
+    </>
+  )
+}
