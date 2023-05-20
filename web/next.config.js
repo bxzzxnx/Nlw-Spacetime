38fb6212de05f5d905393b9afbd0b8e6ef38d53b
Antonio <antonio.bozzano3@hotmail.com>
2023-05-20 19:54:51 -0300

    Day 5 done 👍

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['avatars.githubusercontent.com'], // quais domínios vao ser permitidos
+    domains: ['avatars.githubusercontent.com', '192.168.1.7'], // quais domínios vao ser permitidos
   },
 }
 
